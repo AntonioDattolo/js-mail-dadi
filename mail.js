63f4c47d14e973f4e20f144dae1b136e95640e30
AntonioDattolo <antonio19794.a@gmail.com>
2024-04-22 18:41:43 +0200

    fixed

--- mail.js
+++ mail.js
@@ -1,26 +1,26 @@
 
-let listaMail = [
+const listaMail =  [
     "antonio@ciao.it",
     "paolo@ciao.it",
     "haloa@ciao.com"
 ];
 console.log(listaMail)
 
-let a = listaMail[length]
-
-
 function login() {
-
-    let x = document.getElementById("mail").value
-    if (x != a) {
-        console.log("false")
-        document.getElementById("result").innerHTML = "ACCESS DENIED"
-    } else if (x = a) {
-        console.log("true")
-        document.getElementById("result").innerHTML = "ACCESS GRANTED"
+    
+    
+    let user = document.getElementById("mail").value
+    for(let i = 0; i < listaMail.length; i ++) {
+        let a = listaMail[i] 
+        if (user == a) {
+            console.log("true")
+            document.getElementById("result").innerHTML = "ACCESS GRANTED"
+        }
+        else if (user != a) {
+            console.log("false")
+         document.getElementById("result").innerHTML = "ACCESS DENIED"
+         }
+      
     }
-
-}   
-
-
-
+        
+}
